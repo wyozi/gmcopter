e6f38381080a677fae55fa4efeec4950cf61027c
wyozi <joonasmertanen@gmail.com>
2015-02-17 16:36:51 +0200

    Don't grab ear while talking if we're inside helicopter

--- gamemode/gmcmodules/base/cl_player.lua
+++ gamemode/gmcmodules/base/cl_player.lua
@@ -1,21 +1,18 @@
-function GM:GrabEarAnimation( ply )	
+function GM:GrabEarAnimation( ply )
 
 	ply.ChatGestureWeight = ply.ChatGestureWeight or 0
 
 	-- Don't show this when we're playing a taunt!
-	if ( ply:IsPlayingTaunt() ) then return end
+	if ply:IsPlayingTaunt() then return end
 
-	if ( ply:IsTyping() or ply:IsSpeaking() ) then
+	if not ply:IsInHelicopter() and (ply:IsTyping() or ply:IsSpeaking()) then
 		ply.ChatGestureWeight = math.Approach( ply.ChatGestureWeight, 1, FrameTime() * 5.0 );
 	else
 		ply.ChatGestureWeight = math.Approach( ply.ChatGestureWeight, 0, FrameTime() * 5.0 );
 	end
 
-	if ( ply.ChatGestureWeight > 0 ) then
-
+	if ply.ChatGestureWeight > 0 then
 		ply:AnimRestartGesture( GESTURE_SLOT_VCD, ACT_GMOD_IN_CHAT, true )
 		ply:AnimSetGestureWeight( GESTURE_SLOT_VCD, ply.ChatGestureWeight )
-
 	end
-
 end
