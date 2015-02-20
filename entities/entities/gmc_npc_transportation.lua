913e3e8a42ac4f6a0065eeed797a16d1fb661201
wyozi <joonasmertanen@gmail.com>
2015-02-21 00:06:24 +0200

    Implement proper NPC vehicle entering

--- entities/entities/gmc_npc_transportation.lua
+++ entities/entities/gmc_npc_transportation.lua
@@ -4,34 +4,47 @@ end
 
 ENT.Base = "gmc_npc_base"
 
+function ENT:TickBehaviour()
+    local heli = self:FindHelicopter()
+    local IsInHeli = IsValid(self.InHeli)
+
+    --gmcdebug.Msg(self:GetSequence())
+
+    if not heli and not IsInHeli and self:GetSequence() ~= 626 then -- No idea where 626 is from. Some magic value for sit_ground sequence
+        self:PlaySequenceAndWait( "idle_to_sit_ground" ) 
+        self:SetSequence( "sit_ground" )
+    end
+     
+    if heli and not IsInHeli then
+        if heli:GetPos():Distance(self:GetPos()) > 150 then
+            if self:GetSequence() == 626 then
+                self:PlayScene( "scenes/npc/female01/finally.vcd" )
+                self:PlaySequenceAndWait( "sit_ground_to_idle" )
+            end
+
+            self:StartActivity( ACT_WALK )
+            self.loco:SetDesiredSpeed( 100 )
+            self:MoveToPos(heli:GetPos(), {repath=0.5, tolerance=150})
+            self.loco:FaceTowards(heli:GetPos())
+            self:StartActivity( ACT_IDLE ) 
+            coroutine.wait(0.5)
+        else
+            if not self.InHeli then
+                --PrintTable(self:GetSequenceList())
+                self:SetSequence("silo_sit")
+                self:SetAngles(heli:GetAngles())
+                self:SetPos(heli:GetPos() + heli:GetForward() * 34 + heli:GetUp() * 35 + heli:GetRight() * 15)
+                self:SetParent(heli)
+                self.InHeli = true
+                -- TODO
+            end
+        end
+    end
+end
+
 function ENT:RunBehaviour()
-    while ( true ) do
-
-    	local heli = self:FindHelicopter()
-    	local IsInHeli = IsValid(self.InHeli)
-
-    	--gmcdebug.Msg(self:GetSequence())
-
-    	if not heli and not IsInHeli and self:GetSequence() ~= 626 then -- No idea where 626 is from. Some magic value for sit_ground sequence
-    		self:PlaySequenceAndWait( "idle_to_sit_ground" ) 
-        	self:SetSequence( "sit_ground" )
-    	end
-    	 
-    	if heli and not IsInHeli then
-    		if heli:GetPos():Distance(self:GetPos()) > 200 then
-		        self:PlayScene( "scenes/npc/female01/finally.vcd" )
-        		self:PlaySequenceAndWait( "sit_ground_to_idle" )                            -- Get up
-	    		self:StartActivity( ACT_WALK )                            -- walk anims
-		        self.loco:SetDesiredSpeed( 100 )                        -- walk speeds
-		        self:MoveToPos( heli:GetPos() + Vector( math.Rand( -1, 1 ), math.Rand( -1, 1 ), 0 ) * 200 ) -- walk to a random place within about 200 units (yielding)
-		        self.loco:FaceTowards(heli:GetPos())
-		        self:StartActivity( ACT_IDLE )  
-		    else
-		    	if not self.InHeli then
-		    		-- TODO
-		    	end
-		    end
-    	end
+    while true do
+    	self:TickBehaviour()
         coroutine.yield()
     end
 end
