cba66758f5e56661ebf04d55861e7d30867a3d3b
wyozi <joonasmertanen@gmail.com>
2015-02-22 18:20:04 +0200

    Add resources file. Ruby is gucci

--- content/mass_lowercase.rb
+++
@@ -1,5 +0,0 @@
-Dir.glob("./**/*").each do |file|
-  if file != file.downcase
-  	File.rename(file, file.downcase)
-  end
-end
---
+++ content/prep_resources.rb
@@ -0,0 +1,23 @@
+resources = []
+
+# Rename for Unix compatibility and add files to list of resources
+Dir.glob('**/*').each do |file|
+  if file == 'prep_resources.rb'
+  	next
+  end
+
+  if file != file.downcase
+    File.rename(file, file.downcase)
+  end
+
+  if not File.directory?(file)
+  	resources.push(file)
+  end
+end
+
+File.open('../gamemode/gmcmodules/_gen/sv_resources.lua', 'w') { |file|
+  file.puts '-- Automatically generated resource file. See content/prep_resources.rb'
+  resources.each do |res|
+    file.puts 'resource.AddFile("' + res + '")'
+  end
+}
