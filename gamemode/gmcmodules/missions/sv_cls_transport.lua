b73770226876f5d4f7181ead5572ef4bb0b81544
wyozi <joonasmertanen@gmail.com>
2015-02-26 22:19:04 +0200

    Make transportmission npc think code clearer

--- gamemode/gmcmodules/missions/sv_cls_transport.lua
+++ gamemode/gmcmodules/missions/sv_cls_transport.lua
@@ -16,42 +16,36 @@ function TransportMission:think()
 end
 
 function TransportMission:npc_think(npc)
-	-- TODO find heli from mission helis, not all of them
-	local heli = npc:FindHelicopter()
-	 
-	if heli and not npc:IsInHelicopter() and heli:IsJustAboveGround() then
-		if heli:GetPos():Distance(npc:GetPos()) > 150 then
-			if npc:GetSequence() == 626 then
-				npc:PlayScene( "scenes/npc/female01/finally.vcd" )
-				npc:PlaySequenceAndWait( "sit_ground_to_idle" )
-			end
+	if npc:IsInHelicopter() then
+		local heli = npc:GetHelicopter()
 
-			npc:StartActivity( ACT_WALK )
-			npc.loco:SetDesiredSpeed( 100 )
+		if heli:IsJustAboveGround() and heli:GetPos():Distance(self.targetPOI) < 400 then
+			heli:LeaveHelicopter(npc)
 
-			local oldhelipos = heli:GetPos()
-			local moved = npc:MoveToPos(oldhelipos, {
-				repath=0.5,
-				tolerance=150,
-				terminate_condition = function()
-					return IsValid(heli) and heli:GetPos():Distance(oldhelipos) >= 150
-				end
-			})
-			npc:StartActivity(ACT_IDLE) 
-
-			if moved == "ok" then
-				npc.loco:FaceTowards(heli:GetPos())
-				coroutine.wait(0.25)
-			end
-		else
-			heli:EnterHelicopter(npc)
-			coroutine.wait(1)
+			self:set_accomplished()
 		end
-	end
+	else
+		local heli = npc:FindHelicopter()
+		 
+		if heli and heli:IsJustAboveGround() then
+			if heli:GetPos():Distance(npc:GetPos()) > 150 then
+				npc:StartActivity(ACT_WALK)
+				npc.loco:SetDesiredSpeed( 100 )
 
-	if npc:IsInHelicopter() and npc:GetHelicopter():IsJustAboveGround() and npc:GetHelicopter():GetPos():Distance(self.targetPOI) < 400 then
-		npc:GetHelicopter():LeaveHelicopter(npc)
-
-		self:set_accomplished()
+				local oldhelipos = heli:GetPos()
+				local moved = npc:MoveToPos(oldhelipos, {
+					repath=0.5,
+					tolerance=150,
+					terminate_condition = function()
+						return IsValid(heli) and heli:GetPos():Distance(oldhelipos) >= 150
+					end
+				})
+				npc:StartActivity(ACT_IDLE)
+			else
+				npc.loco:FaceTowards(heli:GetPos())
+				coroutine.wait(0.5)
+				heli:EnterHelicopter(npc)
+			end
+		end
 	end
 end
