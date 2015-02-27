84b877a375074563a1068ccb508ed87f5b4d0574
wyozi <joonasmertanen@gmail.com>
2015-02-27 22:03:19 +0200

    Support space and ctrl for ascending and descending

--- gamemode/gmcmodules/base/cl_keys.lua
+++ gamemode/gmcmodules/base/cl_keys.lua
@@ -4,10 +4,10 @@ function GM:PlayerBindPress(ply, bind, pressed)
 	if not IsValid(ply) then return end
 
 	if ply:IsInHelicopter() then
-		if bind == "+menu" then
+		if bind == "+menu" or bind == "+jump" then
 			RunConsoleCommand(pressed and "+gmc_incalt" or "-gmc_incalt")
 			return true
-		elseif bind == "undo" then
+		elseif bind == "undo" or bind == "+duck" then
 			RunConsoleCommand(pressed and "+gmc_decalt" or "-gmc_decalt")
 			return true
 		elseif bind == "+reload" and pressed then
